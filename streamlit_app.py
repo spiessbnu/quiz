a1535842be6435aee92f62aee1f5d9c9f998bef7
Maiko Rafael Spiess <48365668+spiessbnu@users.noreply.github.com>
2025-06-14 19:57:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     page_title="Quiz",
     page_icon="🧑‍🏫",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # CSS personalizado
